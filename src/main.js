081a883980da50aae5a1dc6905443dcc6da28563
ElizabethChillcce <eli.chillcce.08@gmail.com>
2022-03-09 19:14:46 -0500

    primer avance en js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,24 @@
-import { example } from './data.js';
+import { example,  } from './data.js';
+import athletes from './data/athletes/athletes.js';
 // import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
+console.log(athletes[0]);
+import data from './data/athletes/athletes.js';
 // import data from './data/rickandmorty/rickandmorty.js';
+//console.log(athletes, data);
 
-console.log(example, data);
+function mostrarPagina() {
+    document.getElementById("homePage").style.display="none";
+    document.getElementById("answer").style.display="block";
+}
+
+let buttonUno= document.getElementById("btnQuestions");
+buttonUno.addEventListener("click",mostrarPagina);
+
+let arrayPlay = data. athletes;
+let array = [];
+for (let i=0; i<arrayPlay.length; i++) {
+    
+    array.push( arrayPlay[i]. name);
+     
+ }
+ console.log(array);
