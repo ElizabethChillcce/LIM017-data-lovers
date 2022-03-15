abcfdc52b374c12f0faadaf390137b62dce19156
ElizabethChillcce <eli.chillcce.08@gmail.com>
2022-03-14 21:43:29 -0500

    Avances en data.js

--- src/data.js
+++ src/data.js
@@ -1,21 +1,21 @@
 // estas funciones son de ejemplo
 
-/*export function filterData(data, condition) {
-  console.log(data.filter(arrayPlay));
+export function filterData(data, condition) {
+  console.log(data.filter(arrayPlay));{
   if (data.age>= 30 && data.age < 40) {
     return true;
   }
   return false;
-  
-};*/
+ }
+};
 
-export function sortData(data) {
+/*export function sortData(data) {
   for (let i=0; i<arrayPlay.length -1; i++) {
   console.log(arrayPlay[i].sport);
 } 
-  //console.log(data.sort(arrayPlay));
+  console.log(data.sort(arrayPlay));
 
-}
+}*/
 
 
 //export const sortData = () => {
--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import {sortData} from './data.js';
+import {filterData} from './data.js';
 import data from './data/athletes/athletes.js';
 // import data from './data/lol/lol.js';
 // import data from './data/rickandmorty/rickandmorty.js';
@@ -12,17 +12,16 @@ let buttonUno= document.getElementById("btnQuestions");
 buttonUno.addEventListener("click",mostrarPagina);
 
 const champions = document.getElementById("champions")
-//let arrayPlay = data.athletes;
-//for (let i=0; i<arrayPlay.length; i++) {
+let arrayPlay = data.athletes;
+ for (let i=0; i<arrayPlay.length; i++) {
     
- // let newDiv = document.createElement("div");
-  //  let newContent = document.createTextNode(arrayPlay[i].name);
-  //  newDiv.appendChild(newContent);
-  //  champions.appendChild(newDiv); 
-// }
+    let newDiv = document.createElement("div");
+    let newContent = document.createTextNode(arrayPlay[i].name);
+    newDiv.appendChild(newContent);
+    champions.appendChild(newDiv); 
+ }
 //console.log(filterData(data.athletes));
 
-let arrayPlay = data.athletes;
 
 
 
