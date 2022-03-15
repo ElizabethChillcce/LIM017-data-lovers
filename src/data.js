786f5e9b51ba681260c857d8c00f5868b31b9809
ElizabethChillcce <eli.chillcce.08@gmail.com>
2022-03-15 11:38:08 -0500

    funciòn filter por edad

--- src/data.js
+++ src/data.js
@@ -1,21 +1,24 @@
 // estas funciones son de ejemplo
 
 export function filterData(data, condition) {
-  console.log(data.filter(arrayPlay));{
-  if (data.age>= 30 && data.age < 40) {
-    return true;
-  }
-  return false;
- }
+  // console.log(data.filter(arrayPlay));
+ return data.filter((ath) => {
+    if (ath.age>= 30 && ath.age < 40) {
+      return true;
+    }
+    return false;
+  });
+ 
+  
 };
 
-/*export function sortData(data) {
+export function sortData(data) {
   for (let i=0; i<arrayPlay.length -1; i++) {
   console.log(arrayPlay[i].sport);
 } 
   console.log(data.sort(arrayPlay));
 
-}*/
+}
 
 
 //export const sortData = () => {
--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import {filterData} from './data.js';
 import data from './data/athletes/athletes.js';
 // import data from './data/lol/lol.js';
 // import data from './data/rickandmorty/rickandmorty.js';
-
+console.log(filterData(data.athletes, "condition"));
 function mostrarPagina() {
     document.getElementById("homePage").style.display="none";
     document.getElementById("answer").style.display="block";
@@ -12,11 +12,11 @@ let buttonUno= document.getElementById("btnQuestions");
 buttonUno.addEventListener("click",mostrarPagina);
 
 const champions = document.getElementById("champions")
-let arrayPlay = data.athletes;
+let arrayPlay = filterData(data.athletes, "condition");
  for (let i=0; i<arrayPlay.length; i++) {
     
     let newDiv = document.createElement("div");
-    let newContent = document.createTextNode(arrayPlay[i].name);
+    let newContent = document.createTextNode(arrayPlay[i].age);
     newDiv.appendChild(newContent);
     champions.appendChild(newDiv); 
  }
