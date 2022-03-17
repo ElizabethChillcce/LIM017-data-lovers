a76d501b5bbfd75cc43d1f9a59fbac3f17feaa9d
ElizabethChillcce <eli.chillcce.08@gmail.com>
2022-03-16 22:38:50 -0500

    Diseñando la segunda pàgina

--- src/main.js
+++ src/main.js
@@ -8,8 +8,8 @@ function mostrarPagina() {
     document.getElementById("answer").style.display="block";
 }
 
-//let buttonUno= document.getElementById("btnQuestion");
- //buttonUno.addEventListener("click",mostrarPagina);
+let buttonUno= document.getElementById("buttonOlympics");
+ buttonUno.addEventListener("click",mostrarPagina);
 
 const champions = document.getElementById("champions")
 const selectSports = document.getElementById("selectSport")
@@ -17,13 +17,13 @@ selectSport.addEventListener('change', (e) => {
     console.log(filterData(data.athletes, e.target.value ))
 })
 // let arrayPlay = filterData(data.athletes,'Rowing' );
-//  for (let i=0; i<arrayPlay.length; i++) {
+// for (let i=0; i<arrayPlay.length; i++) {
     
-//     let newDiv = document.createElement("div");
-//     let newContent = document.createTextNode(arrayPlay[i].age);
-//     newDiv.appendChild(newContent);
-//     champions.appendChild(newDiv); 
-//  }
+   // let newDiv = document.createElement("div");
+    // let newContent = document.createTextNode(filterData[i].name.team.age.medal);
+    // newDiv.appendChild(newContent);
+    // champions.appendChild(newDiv); 
+ // }
 //console.log(filterData(data.athletes));
 
 
