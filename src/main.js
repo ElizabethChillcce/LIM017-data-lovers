2031131dbbcfeea5210ab6f95f7163310e564327
ElizabethChillcce <eli.chillcce.08@gmail.com>
2022-03-22 14:05:14 -0500

    primera historia de usuario

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import data from './data/athletes/athletes.js';
 // import data from './data/lol/lol.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 // console.log(filterData(data.athletes, "condition"));
+const arrayPlay = filterData(data.athletes);
+
 function mostrarPagina() {
     document.getElementById("homePage").style.display="none";
     document.getElementById("answer").style.display="block";
@@ -11,20 +13,53 @@ function mostrarPagina() {
 let buttonUno= document.getElementById("buttonOlympics");
  buttonUno.addEventListener("click",mostrarPagina);
 
-const champions = document.getElementById("champions")
+const Datos = document.getElementById("Datos")
+
 const selectSports = document.getElementById("selectSport")
 selectSport.addEventListener('change', (e) => {
-    console.log(filterData(data.athletes, e.target.value ))
+//console.log(filterData(data.athletes, e.target.value ))
+  const resultSport = filterData (data.athletes, e.target.value )
+  const tableBody = document.getElementById("Datos")
+  let htmlrow ="";
+  for (let i=0; i<resultSport.length; i++){
+
+ htmlrow +=
+ `
+  <tr>
+    <td>${resultSport[i].name}</td>
+    <td>${resultSport[i].team}</td>
+    <td>${resultSport[i].age}</td>
+    <td>${resultSport[i].medal}</td>
+    </tr>
+   `; 
+  console.log(htmlrow);
+}
+Datos.innerHTML = htmlrow;
 })
-// let arrayPlay = filterData(data.athletes,'Rowing' );
-// for (let i=0; i<arrayPlay.length; i++) {
+//let tableSport = (mostrar) => {
+/* Datos.innerHTML = ``;
+ mostrar.forEach ((z)=> {
+   Datos.innerHTML += resultSport(z);
+  return(resultSport);
+ });
+ }
+ tableSport(resultSport)
+
+//return (resultSport);*/
+
+ //const arrayPlay = filterData(data.athletes);
+//for (let i=0; i<arrayPlay.length; i++) {
     
-   // let newDiv = document.createElement("div");
-    // let newContent = document.createTextNode(filterData[i].name.team.age.medal);
-    // newDiv.appendChild(newContent);
-    // champions.appendChild(newDiv); 
+ //let newDiv = document.createElement("div");
+ // let newContent = document.createTextNode(arrayPlay[i].name);
+ //   newDiv.appendChild(newContent);
+ //    champions.appendChild(newDiv);
  // }
 //console.log(filterData(data.athletes));
+
+//tabla
+
+
 
 
 
